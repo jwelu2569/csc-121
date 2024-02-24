903f0045c29c342e7d865978a59468edbae11afd
jwelu2569 <158523958+jwelu2569@users.noreply.github.com>
2024-02-23 22:52:05 -0600

    Add files via upload

---
+++ Q7_v1/Q7_v1/AsciiTest.h
@@ -0,0 +1,11 @@
+#pragma once
+class AsciiTest
+{
+private:
+
+public:
+	void doSomething();
+	void convertDigitSmallLetters();
+	void capitalizeSmallLetters(); // a-z => A-Z
+	void smallLettersCapital(); // A-Z => a-z
+};
---
+++ Q7_v1/Q7_v1/EndUserTest.cpp
@@ -0,0 +1,14 @@
+#include "AsciiTest.h"
+using namespace std;
+
+int main()
+{
+	AsciiTest a;
+
+	//a.doSomething();
+	//a.convertDigitSmallLetters();
+	a.capitalizeSmallLetters();
+	a.smallLettersCapital();
+
+	return 0;
+}

f5becc730a5767bdf0794bc9248b738f03115782
jwelu2569 <158523958+jwelu2569@users.noreply.github.com>
2024-04-04 17:07:21 -0500

    Add files via upload

---
+++ Week11_DynamicArray/Week11_DynamicArray/DAExample.h
@@ -0,0 +1,11 @@
+#pragma once
+class DAExample
+{
+private:
+
+public:
+	void generateRN();
+	int readRN();
+	void populateDA(int s); // sends to heap
+	void displayArray(int *myNumbers, int size);
+};
